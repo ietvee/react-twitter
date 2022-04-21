b736716ee7f84600de2ca399bdb5f7f79ec25cf2
Jet Yee <jetyeelow@gmail.com>
2022-04-21 16:12:59 +0800

    signup page styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Signup from "./Signup.js";
 import Main from "./Main.js";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <Signup />
-      <Main />
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
+      {/* <Main /> */}
     </>
   );
 }
